b58b82e6b0b7fb4bbc92dacf06cff6dc75b2f646
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-19 19:54:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   lowest_key = nil
   if !name_hash.empty?
+    lowest_value = name_hash.first[1]
     name_hash.each do |key, value|
       if value < lowest_value
         lowest_value = value
