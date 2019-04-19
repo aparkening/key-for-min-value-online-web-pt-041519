2052b6a7ae033b2d9a04a0509c0f92f2b4922fa0
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-19 21:02:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,12 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value. If empty hash, return nil.
 def key_for_min_value(name_hash)
   lowest_key = nil
-          binding.pry 
-  
+
   if !name_hash.empty?
     lowest_value = name_hash.first[1]
     name_hash.each do |key, value|
       if value < lowest_value
-        
-        binding.pry 
-        
+
         lowest_value = value
         lowest_key = key
       end 
@@ -20,5 +17,3 @@ def key_for_min_value(name_hash)
   
   lowest_key  
 end
-
-key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})
