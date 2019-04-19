c7ea500c8281bf2cb158d26dcd1a33cf0b993676
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-19 19:46:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,7 @@
 # Given a hash with numeric values, return the key for the smallest value. If empty hash, return nil.
 def key_for_min_value(name_hash)
-  if name_hash.empty?
-    lowest_key = nil
-  else
+  lowest_key = nil
+  if !name_hash.empty?
     lowest_value = name_hash.first[1]
     lowest_key = name_hash.first[0]
     name_hash.each do |key, value|
@@ -11,7 +10,6 @@ def key_for_min_value(name_hash)
         lowest_key = key
       end 
     end
-  end
   
   lowest_key  
 end
